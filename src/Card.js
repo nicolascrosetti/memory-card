aebc9f7149b4defffc30189c59280d3447969253
Nicolas <nccrosetti@gmail.com>
2022-06-11 00:16:35 -0300

    Remove files

--- src/Card.js
+++
@@ -1,8 +0,0 @@
-export const Card = ({ card }) => {
-  return (
-    <div className='card'>
-      <img src={card.imageSrc} alt='penguin' className='card-image' />
-      <p>{card.name}</p>
-    </div>
-  );
-};
--- src/CardList.js
+++
@@ -1,7 +0,0 @@
-import { Card } from './Card';
-
-export const CardList = ({ cardsArray }) => {
-  return cardsArray.map((card) => {
-    return <Card key={card.id} card={card} />;
-  });
-};
